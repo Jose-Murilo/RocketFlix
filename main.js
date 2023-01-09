d853ce06450b0ac9b1dc66d1261f698c6c6b07df
Jose Murilo <murilojucas360@hotmail.com>
2023-01-09 13:11:02 -0300

    add modification main.js

--- main.js
+++ main.js
@@ -16,25 +16,24 @@ buttonMovieRandom.addEventListener('click', async () => {
     const id = Math.floor(Math.random() * 500)
 
     const api = `${BASE_URL}${id}?${API_KEY}&${language}`
-    console.log(api);
     const response = await fetch(api).then(response => response.json())
     
     if (response.overview && response.original_title !== "") {
       imgMovie.src = IMG_URL + response.poster_path
       movieTitle.innerHTML = response.original_title
       console.log(movieDescription.innerHTML = response.overview)
-      buttonMovieRandom.style.marginTop = '3.8rem'
+      buttonMovieRandom.style.marginTop = '2.8rem'
 
       
       container.style.height = '100vh'
       main.style.marginTop = '2rem'
     }
 
-    // if (window.innerWidth <= 594) {
-    //   containerTwo.style.height = '100%'
-    // } else {
-    //   containerTwo.style.height = '100vh'
-    // }
+    if (window.innerWidth <= 594) {
+      containerTwo.style.height = '100%'
+    } else {
+      containerTwo.style.height = '100vh'
+    }
 
   })
   
