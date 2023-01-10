24084275504cf578fc628d0aec31cb69c7a18df9
Jose Murilo <murilojucas360@hotmail.com>
2023-01-10 12:07:46 -0300

    add error screen

--- main.js
+++ main.js
@@ -1,32 +1,48 @@
-import {
-  API_KEY, BASE_URL,
-  IMG_URL,
-  language,
-} from './api.js'
+import { API_KEY, BASE_URL, IMG_URL, language } from "./api.js";
 
-const imgMovie = document.querySelector('.imgMovie')
-let movieTitle = document.querySelector('.movieTitle')
-let movieDescription = document.querySelector('.movieDescription')
-const buttonMovieRandom = document.querySelector('.buttonMovieRandom')
-const container = document.querySelector('.container')
-const containerTwo = document.querySelector('.containerTwo')
-const main = document.querySelector('main')
+const imgMovie = document.querySelector(".imgMovie");
+let movieTitle = document.querySelector(".movieTitle");
+let movieDescription = document.querySelector(".movieDescription");
+let releaseDate = document.querySelector(".releaseDate");
+const containerMovie = document.querySelector(".containerMovie");
+const buttonMovieRandom = document.querySelector(".buttonMovieRandom");
+const container = document.querySelector(".container");
+const main = document.querySelector("main");
+const header = document.querySelector("header");
 
-buttonMovieRandom.addEventListener('click', async () => {
-    const id = Math.floor(Math.random() * 500)
-    const api = `${BASE_URL}${id}?${API_KEY}&${language}`
-    const response = await fetch(api).then(response => response.json())
-    
-    if (response.overview && response.original_title !== "") {
-      imgMovie.src = IMG_URL + response.poster_path
-      movieTitle.innerHTML = response.original_title
-      movieDescription.innerHTML = response.overview
-      buttonMovieRandom.style.marginTop = '2.8rem'
-      container.style.height = '100vh'
-      main.style.marginTop = '2rem'
+console.log('O erro no console é por conta do id random que foi criado, pois tem id que a api não tem, daí ela falha na requisição e retorna o erro 404');
+buttonMovieRandom.addEventListener("click", async () => {
+  const id = Math.floor(Math.random() * 550);
+  const api = `${BASE_URL}${id}?${API_KEY}&${language}`;
+  const response = await fetch(api).then((response) => response.json());
+  const contentText = document.querySelector(".contentText");
+
+  const releaseDateYear = new Date(response.release_date).getFullYear()
+  const releaseDateDay = new Date(response.release_date).getDate()
+  const releaseDateMonth = new Date(response.release_date).getMonth()
+
+  if (id === response.id) {
+    if (response.overview && response.title !== "") {
+      contentText.style.display = "block";
+      contentText.style.alignItems = "";
+      header.style.paddingBottom = "0";
+      containerMovie.style.display = "flex";
+      imgMovie.src = IMG_URL + response.poster_path;
+      movieTitle.innerHTML = response.title;
+      movieDescription.innerHTML = response.overview;
+      releaseDate.innerHTML = `<strong>Data: ${releaseDateDay}/${releaseDateMonth}/${releaseDateYear}</strong>`
+
+      buttonMovieRandom.style.marginTop = "2.8rem";
+      container.style.height = "100vh";
+      main.style.marginTop = "2rem";
     }
-  })
-  
-  if (window.innerWidth > 0) {
-    containerTwo.style.height = '100vh'
+  } else {
+    contentText.style.display = "flex";
+    contentText.style.alignItems = "center";
+    imgMovie.src = "./assets/poster.svg";
+    movieTitle.innerHTML = `Ops, hoje não é dia de assistir filme.
+      Bora codar! 🚀`;
+    movieDescription.innerText = "";
+    releaseDate.innerHTML = "";
   }
+});
